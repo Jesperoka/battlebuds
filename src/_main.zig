44f478b60694681dbfcb0d42c7442a4bc910c8dd
Jesperoka <jesperoka@gmail.com>
2024-08-23 17:31:22 +0200

    clean slate

--- src/_main.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-const c_xcb_example = @cImport(@cInclude("xcb_example5.c"));
-
-pub fn main() !void {
-    std.debug.print("\nProgram start!\n", .{});
-    _ = c_xcb_example.run();
-    // std.debug.print("\n{d}\n", .{errno});
-    std.debug.print("\nProgram end!\n", .{});
-}
