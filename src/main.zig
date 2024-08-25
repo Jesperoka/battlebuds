b23a685022e78c074629dadac943dcf4ac5e8250
Jesperoka <jesperoka@gmail.com>
2024-08-25 23:34:50 +0200

    changed function names to follow Zig convention (mixedCase)

--- src/main.zig
+++ src/main.zig
@@ -18,13 +18,13 @@ fn strprint(str: []const u8) void {
 
 // Translated expanded C macros from libpng
 //-----------------------------------------
-fn image_size(image: png.png_image) c_ulong {
+fn imageSize(image: png.png_image) c_ulong {
     return @as(c_ulong, @bitCast(@as(c_ulong, ((if ((@as(c_uint, @bitCast(image.format)) & @as(c_uint, 8)) != 0) @as(c_uint, @bitCast(@as(c_int, 1))) else ((@as(c_uint, @bitCast(image.format)) & @as(c_uint, 4)) >> @intCast(2)) +% @as(c_uint, @bitCast(@as(c_int, 1)))) *% @as(c_uint, @bitCast(image.height))) *% ((if ((@as(c_uint, @bitCast(image.format)) & @as(c_uint, 8)) != 0) @as(c_uint, @bitCast(@as(c_int, 1))) else (@as(c_uint, @bitCast(image.format)) & (@as(c_uint, 2) | @as(c_uint, 1))) +% @as(c_uint, @bitCast(@as(c_int, 1)))) *% @as(c_uint, @bitCast(image.width))))));
 }
-fn image_row_stride(image: png.png_image) c_int {
+fn imageRowStride(image: png.png_image) c_int {
     return @as(c_int, @bitCast((if ((@as(c_uint, @bitCast(image.format)) & @as(c_uint, 8)) != 0) @as(c_uint, @bitCast(@as(c_int, 1))) else (@as(c_uint, @bitCast(image.format)) & (@as(c_uint, 2) | @as(c_uint, 1))) +% @as(c_uint, @bitCast(@as(c_int, 1)))) *% @as(c_uint, @bitCast(image.width))));
 }
-fn image_pixel_size(image: png.png_image) c_int {
+fn imagePixelSize(image: png.png_image) c_int {
     return @as(c_int, @bitCast(if ((@as(c_uint, @bitCast(image.format)) & @as(c_uint, 8)) != 0) @as(c_uint, @bitCast(@as(c_int, 1))) else ((@as(c_uint, @bitCast(image.format)) & (@as(c_uint, 2) | @as(c_uint, 1))) +% @as(c_uint, @bitCast(@as(c_int, 1)))) *% (((@as(c_uint, @bitCast(image.format)) & @as(c_uint, 4)) >> @intCast(2)) +% @as(c_uint, @bitCast(@as(c_int, 1))))));
 }
 //-----------------------------------------
@@ -40,22 +40,22 @@ const Image = struct {
 };
 
 // Based on example.c from libpng. Note: calls malloc
-fn read_png(path: [*:0]const u8, format: c_uint) ReadError!Image {
+fn readPng(path: [*:0]const u8, format: c_uint) ReadError!Image {
     var img: png.png_image = undefined;
     _ = c.memset(&img, 0, @sizeOf(png.png_image));
     img.version = png.PNG_IMAGE_VERSION;
 
     if (png.png_image_begin_read_from_file(&img, path) != 0) {
         img.format = format;
-        const buf = c.malloc(image_size(img));
+        const buf = c.malloc(imageSize(img));
 
         if (buf == null) {
             png.png_image_free(&img);
             return ReadError.OutOfMemory;
         }
         if (png.png_image_finish_read(&img, null, buf, 0, null) != 0) {
-            const pixel_size = image_pixel_size(img);
-            const stride = image_row_stride(img);
+            const pixel_size = imagePixelSize(img);
+            const stride = imageRowStride(img);
             return Image{ .buffer = buf.?, .width = @intCast(img.width), .height = @intCast(img.height), .stride = stride, .bit_depth = 8 * pixel_size };
         } else {
             c.free(buf); // Buffer was allocated, but image read failed.
@@ -65,7 +65,7 @@ fn read_png(path: [*:0]const u8, format: c_uint) ReadError!Image {
     return ReadError.FailedImageRead;
 }
 
-fn parse_keyboard_event(keycode: c_int) bool {
+fn parseKeyboardEvent(keycode: c_int) bool {
     if (keycode == SDL.SDLK_q) return true;
 
     return false;
@@ -100,8 +100,31 @@ const CharacterPositions = struct {
     // }
 };
 
+fn opaqueToAddr(ptr: *anyopaque) usize {
+    return @intFromPtr(@as(*u8, @ptrCast(ptr)));
+}
+
+fn toUsizeChecked(integer: anytype) usize {
+    @setRuntimeSafety(true);
+    return @as(usize, @intCast(integer));
+}
+
+// C-style pointer arithmatic
+fn copyPixels(start_addr_src: usize, start_addr_dest: usize, stride_src: usize, stride_dest: usize, width: usize, height: usize) void {
+    for (0..height) |row| {
+        var ptr_src = @as([*]u32, @ptrFromInt(start_addr_src + row * stride_src));
+        var ptr_dest = @as([*]u32, @ptrFromInt(start_addr_dest + row * stride_dest));
+
+        for (0..width) |_| {
+            ptr_src += 1;
+            ptr_dest += 1;
+            ptr_dest[0] = ptr_src[0];
+        }
+    }
+}
+
 pub fn main() !void {
-    const image: Image = try read_png("assets/first_guy_big.png", png.PNG_FORMAT_RGBA);
+    const image: Image = try readPng("assets/first_guy_big.png", png.PNG_FORMAT_RGBA);
 
     if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO | SDL.SDL_INIT_EVENTS | SDL.SDL_INIT_AUDIO) < 0)
         sdlPanic();
@@ -125,7 +148,7 @@ pub fn main() !void {
     var b_mask: u32 = undefined; //0x00FF0000;
     var a_mask: u32 = undefined; //0xFF000000;
 
-    const format = SDL.SDL_PIXELFORMAT_BGRA8888;
+    const format = SDL.SDL_PIXELFORMAT_ABGR8888;
     _ = SDL.SDL_PixelFormatEnumToMasks(format, @constCast(&image.bit_depth), @constCast(&r_mask), @constCast(&g_mask), @constCast(&b_mask), @constCast(&a_mask));
 
     const surface: *SDL.SDL_Surface = SDL.SDL_CreateRGBSurfaceFrom(image.buffer, image.width, image.height, image.bit_depth, image.stride, r_mask, g_mask, b_mask, a_mask).?;
@@ -135,39 +158,27 @@ pub fn main() !void {
     const texture: *SDL.SDL_Texture = SDL.SDL_CreateTexture(renderer, format, SDL.SDL_TEXTUREACCESS_STREAMING, image.width, image.height).?;
 
     const src_rect: *SDL.SDL_Rect = @constCast(&.{ .x = 0, .y = 0, .w = image.width, .h = image.height });
-    const dst_rect: *SDL.SDL_Rect = @constCast(&.{ .x = 50, .y = 50, .w = image.width, .h = image.height });
+    const dst_rect: *SDL.SDL_Rect = @constCast(&.{ .x = 1920 / 2 - 50, .y = 1080 / 2 - 50, .w = image.width, .h = image.height });
 
-    var pixels: ?*c_int = undefined; // This is some dumb C shit, holy hell.
+    // This is some dumb C shit, holy hell.
+    var pixels: ?*c_int = undefined;
     const pixels_ptr: [*]?*anyopaque = @ptrCast(@alignCast(@constCast(&pixels)));
 
     var stride: c_int = undefined;
     const stride_ptr: [*]c_int = @ptrCast(@alignCast(@constCast(&stride)));
 
-    // print(SDL.SDL_UpdateTexture(texture, src_rect, surface.pixels, surface.pitch));
-
     print(SDL.SDL_LockTexture(texture, src_rect, pixels_ptr, stride_ptr));
 
-    // Do stuff with pixels
+    const stride_gpu = toUsizeChecked(stride);
+    const start_addr_gpu = opaqueToAddr(pixels_ptr[0].?);
 
-    const gpu_pixel: [*]?*anyopaque = pixels_ptr;
-    const start_addr = @intFromPtr(@as(*u8, @ptrCast(gpu_pixel[0].?)));
+    const stride_cpu = toUsizeChecked(image.stride);
+    const start_addr_cpu = opaqueToAddr(image.buffer.?);
 
-    { // function // TODO: after it works, try replacing the column loop with slicing
-        // const cpu_pixel: [*]u8 = @constCast(&image.buffer[0]);
+    const width = toUsizeChecked(image.width);
+    const height = toUsizeChecked(image.height);
 
-        for (0..@intCast(image.height)) |row| {
-            // 32 bit rows, so we point to start of row.
-            var ptr = @as([*]u32, @ptrFromInt(start_addr + row * @as(usize, @intCast(stride))));
-
-            for (0..@intCast(image.width)) |_| {
-
-                // gpu_pixel = 0xFF000000 |
-                ptr += 1;
-                ptr[0] = 0x00_00_FF_00;
-                // gpu_pixel = @as([*]u8, @ptrCast(cpu_pixel))[idx];
-            }
-        }
-    }
+    copyPixels(start_addr_cpu, start_addr_gpu, stride_cpu, stride_gpu, width, height);
 
     SDL.SDL_UnlockTexture(texture);
 
@@ -179,7 +190,7 @@ pub fn main() !void {
                 SDL.SDL_QUIT => break :main_loop,
                 SDL.SDL_KEYDOWN => {
                     const keyboard_event: *SDL.SDL_KeyboardEvent = @ptrCast(&event);
-                    if (parse_keyboard_event(keyboard_event.keysym.sym)) break :main_loop;
+                    if (parseKeyboardEvent(keyboard_event.keysym.sym)) break :main_loop;
                 },
 
                 else => {},
